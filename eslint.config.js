eca15d5efff638c34580c6c5bf8ee2afd8d5a520
Abianv101 <717821s101@kce.ac.in>
2025-05-07 14:02:38 +0530

    Describe what you changed in this commit

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,19 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+
+      // Allows eslint-disable comments to work for no-unused-vars
+      'no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: true,
+          varsIgnorePattern: '^_',   // allow _myVar
+          argsIgnorePattern: '^_'    // allow function(_, _args)
+        }
+      ],
+
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
